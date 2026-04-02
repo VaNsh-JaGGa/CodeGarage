a5bf29e980b2177a7dec4a39f2e38ef5f46a33f2
VaNsh-JaGGa <vanshjagga66@gmail.com>
2026-04-02 17:57:08 +0530

    blog-app-started

--- ExpressJS/AuthenUsingSequelize/controller/authController.js
+++ ExpressJS/AuthenUsingSequelize/controller/authController.js
@@ -14,9 +14,9 @@ exports.register = async (req, res) => {
             id: user.id,
             email: user.email
         });
-
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        console.log(err);
+        res.status(500).json({ error: "nahi hora register yarr" });
     }
 };
 
@@ -40,6 +40,6 @@ exports.login = async (req,res) => {
         );
         res.json({ token });
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ error: "nahi hora login yarr" });
     }
 };
